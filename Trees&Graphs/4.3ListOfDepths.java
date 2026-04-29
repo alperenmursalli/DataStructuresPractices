5dbaa9fecefa5e823b76fc2ed7912d8ebc18a7ae
Alperen Mursalli <alperenmursalli0@gmail.com>
2026-04-29 15:08:17 +0300

    fix

---
+++ Trees&Graphs/4.3ListOfDepths.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class ListOfDepths {
+     class TreeNode {
+    public int data;      // Düğümün içinde tutulan veri
+    public TreeNode left;  // Sol çocuk
+    public TreeNode right; // Sağ çocuk
+
+    // Kurucu metod (Constructor)
+    public TreeNode(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+    public ArrayList<LinkedList<TreeNode>> createLevelLinkedList(TreeNode root) {
+        ArrayList<LinkedList<TreeNode>> levels = new ArrayList<LinkedList<TreeNode>>();
+
+        LinkedList<TreeNode> currentLevel = new LinkedList<TreeNode>();
+
+
+    }
+
+    
+}
