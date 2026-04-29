328c36bdf6c144bc72f1af2a0fc02d4f79639298
Alperen Mursalli <alperenmursalli0@gmail.com>
2026-04-29 15:19:38 +0300

    List Of Depths

--- Trees&Graphs/4.3ListOfDepths.java
+++ Trees&Graphs/4.3ListOfDepths.java
@@ -1,25 +1,65 @@
 import java.util.*;
 
 class ListOfDepths {
-     class TreeNode {
-    public int data;      // Düğümün içinde tutulan veri
-    public TreeNode left;  // Sol çocuk
-    public TreeNode right; // Sağ çocuk
+    class TreeNode {
+        public int data;    
+        public TreeNode left;  
+        public TreeNode right; 
 
-    // Kurucu metod (Constructor)
-    public TreeNode(int data) {
-        this.data = data;
-        this.left = null;
-        this.right = null;
+        public TreeNode(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
     }
-}
+
     public ArrayList<LinkedList<TreeNode>> createLevelLinkedList(TreeNode root) {
         ArrayList<LinkedList<TreeNode>> levels = new ArrayList<LinkedList<TreeNode>>();
 
         LinkedList<TreeNode> currentLevel = new LinkedList<TreeNode>();
 
+        if (root != null) {
+            currentLevel.add(root);
+        }
 
+        while (!currentLevel.isEmpty()){
+            levels.add(currentLevel);
+
+            LinkedList<TreeNode> parents = currentLevel;
+            currentLevel = new LinkedList<>();
+
+            for (TreeNode node : parents){
+                if (node.left!=null){
+                    currentLevel.add(node.left);
+                }
+                if (node.right != null){
+                    currentLevel.add(node.right);
+                }
+            }
+        }
+        return levels;
     }
 
-    
+    public static void main(String[] args) {
+        ListOfDepths obj = new ListOfDepths();
+
+        TreeNode root = obj.new TreeNode(1);
+        root.left = obj.new TreeNode(2);
+        root.right = obj.new TreeNode(3);
+        root.left.left = obj.new TreeNode(4);
+        root.left.right = obj.new TreeNode(5);
+        root.right.right = obj.new TreeNode(6);
+
+        ArrayList<LinkedList<TreeNode>> levels = obj.createLevelLinkedList(root);
+
+        for (int i = 0; i < levels.size(); i++) {
+            System.out.print("Level " + i + ": ");
+
+            for (TreeNode node : levels.get(i)) {
+                System.out.print(node.data + " ");
+            }
+
+            System.out.println();
+        }
+    }
 }
